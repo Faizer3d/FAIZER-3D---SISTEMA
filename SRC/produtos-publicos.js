d346abfb8caff44026b3517db72a75d2317609b4
Faizer3d <feliperibeiro878@gmail.com>
2026-08-05 19:19:51 -0300

    Update produtos-publicos.js

--- SRC/produtos-publicos.js
+++ SRC/produtos-publicos.js
@@ -19,9 +19,22 @@ window.FAIZER3D_PRODUTOS_PUBLICOS = [
 
         estoque: 1,
 
-        descricao: "Pikachu personalizado produzido em impressão 3D.",
+        descricao:
+            "Pikachu personalizado produzido em impressão 3D.",
 
-        imagens: []
+        imagens: [
+
+            "../Captura de tela 2026-07-23 025900.png",
+
+            "../Captura de tela 2026-07-23 025939.png",
+
+            "../Captura de tela 2026-07-23 025955.png",
+
+            "../Captura de tela 2026-07-23 030010.png",
+
+            "../Captura de tela 2026-07-23 030015.png"
+
+        ]
     }
 
 ];
